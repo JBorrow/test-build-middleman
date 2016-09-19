e35f35d85c33262579de5e87256b78b1d383a738
Josh Borrow <josh.borrow@gmail.com>
2016-09-19 09:58:58 +0100

    fixd, but still no pretty

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -5,24 +5,24 @@ var lunrData  = null;
 
 // Download index data
 $.ajax({
-  url: '/index.json',
+  url: './index.json',
   cache: true,
   method: 'GET',
   success: function(data) {
     lunrData = data;
+	lunrIndex = lunr.Index.load(lunrData.index);
+
+	var $_GET = {};
+
+	document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+		function decode(s) {
+			return decodeURIComponent(s.split("+").join(" "));
+		}
+
+		$_GET[decode(arguments[1])] = decode(arguments[2]);
+	});
+
+	console.log(lunrIndex.search($_GET['search']))
   }
 });
 
-lunrIndex = lunrData[0];
-
-var $_GET = {};
-
-document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
-    function decode(s) {
-        return decodeURIComponent(s.split("+").join(" "));
-    }
-
-    $_GET[decode(arguments[1])] = decode(arguments[2]);
-});
-
-console.log(lunrIndex.search($_GET['search']))
