bc88109bcc1293267d66757ed9595d43f8b4a5b4
Josh Borrow <joshua.borrow@durham.ac.uk>
2016-09-19 09:17:42 +0100

    minor

--- config.rb
+++ config.rb
@@ -49,3 +49,12 @@ activate :search do |search|
     url:     {index: false, store: true}
   }
 end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
