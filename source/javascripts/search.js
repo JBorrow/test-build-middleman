9f3345aca0bf837704f4f90c2ded7790e497c6be
Josh Borrow <josh.borrow@gmail.com>
2016-09-26 10:41:12 +0100

    update

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -1,41 +1,28 @@
 //= require lunr.min
 
-var searchIndexUrl = './index.json';
+var lunrIndex = null;
+var lunrData  = null;
 
-jQuery(function($) {
-    var index,
-        store,
-        data = $.getJSON(searchIndexUrl);
-
-    data.then(function(data){
-        store = data.docs,
-        // create index
-        index = lunr.Index.load(data.index)
-    });
-
-    $('#search-field').keyup(function() {
-        var query = $(this).val();
-        if(query === ''){
-            jQuery('#search-results').empty();
-        }
-        else {
-            // perform search
-            var results = index.search(query);
-            data.then(function(data) {
-                $('#search-results').empty().append(
-                    results.length ?
-                    results.map(function(result){
-						ourinfo = store[result.ref];
-						console.log(ourinfo);
-                        var el = $('<p>')
-                            .append($('<a>')
-                                .attr('href', ourinfo.url)
-                                .text(ourinfo.title)
-                            );
-						return el;
-                    }) : $('<p><strong>No results found</strong></p>')
-                );
-            }); 
-        }
-    }); 
+// Download index data
+$.ajax({
+  url: '/index.json',
+  cache: true,
+  method: 'GET',
+  success: function(data) {
+    lunrData = data;
+  }
 });
+
+lunrIndex = lunrData[0];
+
+var $_GET = {};
+
+document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+    function decode(s) {
+        return decodeURIComponent(s.split("+").join(" "));
+    }
+
+    $_GET[decode(arguments[1])] = decode(arguments[2]);
+});
+
+console.log(lunrIndex.search($_GET['search']))
